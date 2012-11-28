53ef1947ef508801e793789937ce1bf453de7c98
Zolli <zolli07@gmail.com>
2012-11-28 10:09:27 +0100

    *Added some basic class

--- src/com/Zolli/EnderCore/EnderCore.java
+++ src/com/Zolli/EnderCore/EnderCore.java
@@ -1,5 +1,39 @@
 package com.Zolli.EnderCore;
 
-public class EnderCore {
+import java.util.logging.Logger;
 
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnderCore extends JavaPlugin {
+	
+	private PluginManager pluginManager;
+	public Logger log;
+	public String logPrefix;
+	private PluginDescriptionFile pluginDescription;
+	
+	public void onLoad() {
+		
+	}
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public EnderCore getCore() {
+		return this;
+	}
+	
+	public PluginManager getPluginManager() {
+		return this.pluginManager;
+	}
+	
+	public PluginDescriptionFile getDescriptionFile() {
+		return this.pluginDescription;
+	}
 }
