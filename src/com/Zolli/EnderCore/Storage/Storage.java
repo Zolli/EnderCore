31d7f1a57519317a37766aa660cc4b842777835a
Zolli <zolli07@gmail.com>
2012-11-29 21:30:14 +0100

    *Implementing network utils in storageEngine class, this class now able to download driver if needed

--- src/com/Zolli/EnderCore/Storage/Storage.java
+++ src/com/Zolli/EnderCore/Storage/Storage.java
@@ -1,25 +1,43 @@
 package com.Zolli.EnderCore.Storage;
 
+import com.Zolli.EnderCore.Utils.networkUtils;
+
 public class Storage {
 	
-	public void Storage(storageEngine e) {
-		
+	public void Storage(storageEngine engine) {
+		if(!engine.getDownloadUrl().equalsIgnoreCase("none")) {
+			this.downloadDriver(engine.getDownloadUrl(), engine.getFileName());
+		}
+	}
+	
+	private void downloadDriver(String Url, String savedFileName) {
+		networkUtils.downloadAndSave(Url, "./" + savedFileName);
 	}
 	
 	public enum storageEngine {
-		SQLITE("http://"),
-		MySQL("HTTP://"),
+		SQLITE("http://", "sqlite.jar"),
+		MySQL("http://", "mysql.jar"),
+		H2DB("http://", "h2.jar"),
 		FLATFILE("none"),
 		NBT("none");
 		
 		private String driverUrl;
+		private String fileName;
 		
 		private storageEngine(String s) {
 			this.driverUrl = s;
 		}
 		
+		private storageEngine(String s, String f) {
+			this.driverUrl = s;
+		}
+		
 		public String getDownloadUrl() {
 			return this.driverUrl;
+		}
+		
+		public String getFileName() {
+			return this.fileName;
 		}
 		
 	}
