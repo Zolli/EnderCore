decfe88da7fb583eeed4f0552fbd52f4dc97eecf
Zolli <zolli07@gmail.com>
2012-11-30 12:03:13 +0100

    *Working SQL connection both MySQL and SQLite

--- src/com/Zolli/EnderCore/Storage/Storage.java
+++ src/com/Zolli/EnderCore/Storage/Storage.java
@@ -1,17 +1,77 @@
 package com.Zolli.EnderCore.Storage;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import com.Zolli.EnderCore.Logger.simpleLogger;
 import com.Zolli.EnderCore.Utils.networkUtils;
 
 public class Storage {
 	
-	public void Storage(storageEngine engine) {
+	private networkUtils nu;
+	private storageEngine selectedEngine;
+	private String dataFolder;
+	private FileConfiguration config;
+	private simpleLogger logger;
+	
+	public void Storage(storageEngine engine, String dataFolder, FileConfiguration config, simpleLogger log) {
+		this.nu = new networkUtils();
+		this.selectedEngine = engine;
+		this.config = config;
+		this.logger= log;
+		this.dataFolder = dataFolder;
+		
 		if(!engine.getDownloadUrl().equalsIgnoreCase("none")) {
 			this.downloadDriver(engine.getDownloadUrl(), engine.getFileName());
 		}
 	}
 	
+	private void initializeDriver() {
+		if(this.selectedEngine.equals(storageEngine.MySQL)) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else if(this.selectedEngine.equals(storageEngine.SQLITE)) {
+			try {
+				Class.forName("org.slite.JDBC");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private Connection getConnection() {
+		if(this.selectedEngine.equals(storageEngine.MySQL)) {
+			try {
+				Connection conn = DriverManager.getConnection(this.config.getString("database.host"), this.config.getString("database.username"), this.config.getString("database.password"));
+				return conn;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else if(this.selectedEngine.equals(storageEngine.SQLITE)) {
+			try {
+				Connection conn = DriverManager.getConnection("jdbc:sqlite:" + this.dataFolder + File.separator + "EnderCore.db");
+				return conn;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
 	private void downloadDriver(String Url, String savedFileName) {
-		networkUtils.downloadAndSave(Url, "./" + savedFileName);
+		try {
+			this.nu.downloadAndSave(Url, "./" + savedFileName);
+		} catch (Exception e) {
+			e.getMessage();
+			e.printStackTrace();
+		}
 	}
 	
 	public enum storageEngine {
