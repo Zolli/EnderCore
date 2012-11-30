498edd28d279fb982e034b07ca72057ce603ab82
Zolli <zolli07@gmail.com>
2012-11-30 15:10:40 +0100

    *Simple map deserialization

---
+++ src/com/Zolli/EnderCore/Utils/tagHelper.java
@@ -0,0 +1,19 @@
+package com.Zolli.EnderCore.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class tagHelper {
+
+	public Map createMap(String s) {
+		Map<String,String> m = new HashMap<String,String>();
+		String[] pairs = s.split(";");
+		
+		for(String pair : pairs) {
+			String[] kvs = pair.split("=");
+			m.put(kvs[0], kvs[1]);
+		}
+		return m;
+	}
+	
+}
