669ac790d34c4586630e1371d02a8b07c05b27f1
Zolli <zolli07@gmail.com>
2013-02-19 07:52:21 +0100

    *Only pushing changes

--- src/com/Zolli/EnderCore/Commands/command/infoCommand.java
+++ src/com/Zolli/EnderCore/Commands/command/infoCommand.java
@@ -1,5 +1,6 @@
 package com.Zolli.EnderCore.Commands.command;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
@@ -9,8 +10,36 @@ import com.Zolli.EnderCore.Commands.ECCommand;
 
 public class infoCommand implements ECCommand {
 	
+	private List<String> permissions = new ArrayList<String>();
+	
 	public infoCommand() {
-		//this.Permissions.add("ec.basic.info");
+		this.permissions.add("ec.basic.info");
+		this.permissions.add("ec.info");
+	}
+	
+	@Override
+	public String getName() {
+		return "info";
+	}
+
+	@Override
+	public boolean isAccessibleFromConsole() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabledForOPs() {
+		return true;
+	}
+
+	@Override
+	public List<String> getPermission() {
+		return this.permissions;
+	}
+
+	@Override
+	public int getArgsLength() {
+		return 0;
 	}
 	
 	@Override
@@ -18,31 +47,6 @@ public class infoCommand implements ECCommand {
 		Player pl = (Player) sender;
 		pl.sendMessage("Hello");
 		return false;
-	}
-
-	@Override
-	public boolean isAccessibleFromConsole() {
-		return true;
-	}
-
-	@Override
-	public List<String> getPermission() {
-		return null;
-	}
-
-	@Override
-	public int getArgsLength() {
-		return 1;
-	}
-
-	@Override
-	public boolean isEnabledForOPs() {
-		return true;
-	}
-
-	@Override
-	public String getName() {
-		return "info";
 	}
 	
 }
--- src/com/Zolli/EnderCore/Commands/commandHandler.java
+++ src/com/Zolli/EnderCore/Commands/commandHandler.java
@@ -1,35 +1,41 @@
 package com.Zolli.EnderCore.Commands;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 
 import com.Zolli.EnderCore.EnderCore;
 
 
 public class commandHandler implements CommandExecutor {
 
-	private Map<String, ECCommand> handletCommands = new HashMap<String, ECCommand>();
+	private Map<String, ECCommand> handledCommands = new HashMap<String, ECCommand>();
 	private EnderCore plugin;
+	private List<String> commandPermissions;
 	
 	public commandHandler(EnderCore instance) {
 		this.plugin = instance;
 	}
 	
 	public void registerCommand(String name, ECCommand command) {
-		this.handletCommands.put(name, command);
+		this.handledCommands.put(name, command);
 	}
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		System.out.println(arg1.getName());
-		System.out.println(arg3.length);
-		System.out.println(arg2);
-		ECCommand command = this.handletCommands.get(arg3[0]);
-		command.execute(arg0, arg3);
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
+		ECCommand command = this.handledCommands.get(arg3[0]);
+		this.commandPermissions = command.getPermission();
+		
+		if((sender instanceof ConsoleCommandSender) && command.isAccessibleFromConsole()) {
+			
+		}
+		
+		
 		return false;
 	}
 	
