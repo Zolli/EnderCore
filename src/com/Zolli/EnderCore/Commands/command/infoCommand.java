7f511e0c7dff53eb796e4bfd898e19b02ab33f7d
Zolli <zolli07@gmail.com>
2013-02-20 22:08:19 +0100

    *Missing changes magically fix the build errors :)

--- src/com/Zolli/EnderCore/Commands/command/infoCommand.java
+++ src/com/Zolli/EnderCore/Commands/command/infoCommand.java
@@ -5,20 +5,52 @@ import java.util.List;
 
 import org.bukkit.command.CommandSender;
 
+import com.Zolli.EnderCore.EnderCore;
 import com.Zolli.EnderCore.Commands.ECCommand;
 import com.Zolli.EnderCore.Commands.consoleMenuBuilder;
 
 public class infoCommand implements ECCommand {
 	
+	/**
+	 * List of required permission node
+	 */
 	private List<String> permissions = new ArrayList<String>();
+	
+	/**
+	 * List off command examples
+	 */
 	private List<String> examples = new ArrayList<String>();
 	
-	public infoCommand() {
+	/**
+	 * Main class instance
+	 */
+	private EnderCore plugin;
+	
+	/**
+	 * Constructor
+	 * @param instance Main class instance
+	 */
+	public infoCommand(EnderCore instance) {
+		this.plugin = instance;
 		this.permissions.add("ec.basic.info");
 		this.permissions.add("ec.info");
 		
 		this.examples.add("/ec info [param1] [param2]");
 		this.examples.add("/ec info [param1] <param2>");
+	}
+	
+	@Override
+	public boolean execute(CommandSender sender, String[] allArgs, String chainedParams) {
+		consoleMenuBuilder m = new consoleMenuBuilder("EnderCore - Info");
+		m.addMenuItem("/ec info - Display this message");
+		m.addMenuItem("/ec help - Display short help message");
+		m.addMenuItem("/ec purge - Pureg all data in database");
+		m.addMenuItem("/ec status - Display status info");
+		
+		m.build(sender);
+		
+		System.out.println(chainedParams);
+		return false;
 	}
 	
 	@Override
@@ -43,20 +75,6 @@ public class infoCommand implements ECCommand {
 	
 	@Override
 	public boolean isAccessibleFromConsole() {
-		return false;
-	}
-	
-	@Override
-	public boolean execute(CommandSender sender, String[] allArgs, String chainedParams) {
-		consoleMenuBuilder m = new consoleMenuBuilder("EnderCore - Info");
-		m.addMenuItem("/ec info - Display this message");
-		m.addMenuItem("/ec help - Display short help message");
-		m.addMenuItem("/ec purge - Pureg all data in database");
-		m.addMenuItem("/ec status - Display status info");
-		
-		m.build(sender);
-		
-		System.out.println(chainedParams);
 		return false;
 	}
 	
